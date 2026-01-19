8c3f7a6f78f0d37fc688e209d1f3f910fa21ab44
Jawad Ahmad <jawadprod1999@gmail.com>
2026-01-19 22:14:56 +0500

    Adding new graphs

--- app.py
+++ app.py
@@ -333,7 +333,7 @@ try:
             # Keep district order strictly by PM2.5 (descending)
             top_10["district"] = pd.Categorical(
                 top_10["district"],
-                categories=top_10.sort_values("pm2_5", ascending=False)["district"].tolist(),
+                categories=top_10.sort_values("pm2_5", ascending=True)["district"].tolist(),
                 ordered=True,
             )
 
